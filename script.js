582044f20af594c8877ddc630db4710cfcb0e756
sundartalpade <prachitalpade557@gmail.com>
2023-09-06 20:08:54 +0530

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+function getUsers(pageNumber){
+    return fetch(`https://regres.in/api/users?page=${pageNumber}`)
+    .then(function(resonse){
+        return Response.json()
+    })
 
-console.log("hello")
+.then(function(response){
+    console.log(response.data[0].id)
+    return response.data[0].id
+})
+}
 
-
-
-
+function getSingle(id){
+    return fetch(`https://regres.in/api/users/${id}`)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(response){
+        return response.data
+    })
+}
 
 
 
